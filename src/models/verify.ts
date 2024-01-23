22cb9115e09d0724b191c80ddcd0c6fe8fe4d6b9
Dagadev <gacub20ru@gmail.com>
2024-01-23 10:48:41 -0600

    Add verify model

---
+++ src/models/verify.ts
@@ -0,0 +1,7 @@
+import { model, Schema } from 'mongoose'
+
+export const VerifyModel = model('verify', new Schema({
+  guildId: { type: String, required: true },
+  requiredGuildId: { type: String, required: true },
+  rolId: { type: String, required: true }
+}))

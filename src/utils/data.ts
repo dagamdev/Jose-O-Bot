92ef2ac8630cafca91af8978d1afa3d6e8076b6a
Dagadev <gacub20ru@gmail.com>
2024-01-24 12:17:59 -0600

    Add id in bot data & add cache

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,3 +1,12 @@
 export const BOT_DATA = {
+  id: '935927715159236638'
+}
 
+const guildInvites: Array<{
+  guildId: string
+  inviteUrl: string
+}> = []
+
+export const cache = {
+  guildInvites
 }

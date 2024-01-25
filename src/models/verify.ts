7323611e8cf6a920fb6741c055bafc42b828f54d
Dagadev <gacub20ru@gmail.com>
2024-01-24 21:10:47 -0600

    Add inviteUrl property in Verify model

--- src/models/verify.ts
+++ src/models/verify.ts
@@ -3,5 +3,6 @@ import { model, Schema } from 'mongoose'
 export const VerifyModel = model('verify', new Schema({
   guildId: { type: String, required: true },
   requiredGuildId: { type: String, required: true },
-  rolId: { type: String, required: true }
+  rolId: { type: String, required: true },
+  inviteUrl: { type: String, required: true }
 }))

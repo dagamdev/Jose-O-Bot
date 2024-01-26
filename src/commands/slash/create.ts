12a678f4f9c600f9c6524b2e2a3c9cb7b34d92e3
Dagadev <gacub20ru@gmail.com>
2024-01-26 16:26:26 -0600

    Add create slash command

---
+++ src/commands/slash/create.ts
@@ -0,0 +1,52 @@
+import { type BotClient, ClientSlashCommand, type SlashInteraction } from '../../client'
+import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } from 'discord.js'
+import { CUSTOM_IDS } from '../../utils/constants'
+
+export default class CreateSlashCommand extends ClientSlashCommand {
+  constructor () {
+    super(new SlashCommandBuilder()
+      .setName('create')
+      .setNameLocalization('es-ES', 'crear')
+      .setDescription('Create command')
+      .setDescriptionLocalization('es-ES', 'Comando de crear')
+      .addSubcommand(backup => backup
+        .setName('backup')
+        .setNameLocalization('es-ES', 'respaldo')
+        .setDescription('Create server backup.')
+        .setDescriptionLocalization('es-ES', 'Crear respaldo del servidor.')
+      )
+      .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+      .toJSON()
+    )
+  }
+
+  public async execute (int: SlashInteraction, client: BotClient) {
+    const subCommandName = int.options.getSubcommand(true)
+
+    if (subCommandName === 'backup') {
+      const ConfirmationBackupEmbed = new EmbedBuilder({
+        title: '⚠️ ¿Estás seguro de que deseas hacer un respaldo del servidor?',
+        description: 'El proceso tomará un tiempo dependiendo de la cantidad de canales y roles que tenga el servidor.'
+      }).setColor('Yellow')
+
+      const ConfirmationBackupButtons = new ActionRowBuilder<ButtonBuilder>({
+        components: [
+          new ButtonBuilder({
+            customId: CUSTOM_IDS.CREATE_BACKUP_CONFIRM,
+            emoji: '✅',
+            label: 'Confirmar',
+            style: ButtonStyle.Success
+          }),
+          new ButtonBuilder({
+            customId: CUSTOM_IDS.CREATE_BACKUP_CANCEL,
+            emoji: '❌',
+            label: 'Cancelar',
+            style: ButtonStyle.Danger
+          })
+        ]
+      })
+
+      int.reply({ ephemeral: true, embeds: [ConfirmationBackupEmbed], components: [ConfirmationBackupButtons] })
+    }
+  }
+}

03aff0e422d58f5426e87df0f84c58401dc22bbd
Dagadev <gacub20ru@gmail.com>
2024-01-26 16:26:50 -0600

    Add ignore slash command

---
+++ src/commands/slash/ignore.ts
@@ -0,0 +1,38 @@
+import { type BotClient, ClientSlashCommand, type SlashInteraction } from '../../client'
+import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js'
+
+const IgnoreScb = new SlashCommandBuilder()
+  .setName('ignore')
+  .setNameLocalization('es-ES', 'ignorar')
+  .setDescription('Ignore main command')
+  .setDescriptionLocalization('es-ES', 'Comando principal de ignorar')
+  .addSubcommand(list => list
+    .setName('list')
+    .setNameLocalization('es-ES', 'lista')
+    .setDescription('List of ignored channels.')
+    .setDescriptionLocalization('es-ES', 'Lista de canales ignorados.')
+  )
+  .addSubcommand(add => add
+    .setName('add')
+    .setNameLocalization('es-ES', 'agregar')
+    .setDescription('Add a new channel to the ignore list.')
+    .setDescriptionLocalization('es-ES', 'Agrega un nuevo canal a la lista de ignorados.')
+  )
+  .addSubcommand(remove => remove
+    .setName('remove')
+    .setNameLocalization('es-ES', 'eliminar')
+    .setDescription('Remove a channel from the ignore list.')
+    .setDescriptionLocalization('es-ES', 'Eliminar un canal de la lista de ignorados.')
+  )
+  .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+  .toJSON()
+
+export default class BackupSlashCommand extends ClientSlashCommand {
+  constructor () {
+    super(IgnoreScb)
+  }
+
+  public async execute (int: SlashInteraction, client: BotClient) {
+
+  }
+}

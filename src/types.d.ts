898d7cc8ee24455e4700d24d5f1936bd878237e2
Dagadev <gacub20ru@gmail.com>
2024-01-27 13:23:26 -0600

    Add buttonIDKeys type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,6 @@
 import type { ClientEvents } from 'discord.js'
+import type { BUTTON_IDS } from './utils/constants'
 
 export type EventNames = keyof ClientEvents
+
+export type ButtonIDKeys = keyof typeof BUTTON_IDS

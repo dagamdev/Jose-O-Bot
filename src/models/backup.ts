4e883ef41574d121203e34aaff085009d3963377
Dagadev <gacub20ru@gmail.com>
2024-01-27 21:59:03 -0600

    Add timestamps config & Add interfaces

--- src/models/backup.ts
+++ src/models/backup.ts
@@ -1,19 +1,61 @@
-import { model, Schema, Types } from 'mongoose'
+import { model, Schema, type Types } from 'mongoose'
+import { generateCode } from '../lib'
 
-function generateCode (length = 6) {
-  let res = ''
-
-  for (let i = 0; i < length; i++) {
-    res += Math.floor(Math.random() * 22).toString(22)
-  }
-
-  return res
+interface Guild {
+  id: string
+  name: string
+  description: string | null
 }
 
-const ChannelSchema = new Schema({
+const GuildSchema = new Schema<Guild>({
+  id: { type: String, required: true },
+  name: { type: String, required: true },
+  description: String
+})
+
+interface Role {
+  oldId: string
+  name: string
+  color: number
+  hoist: boolean
+  rawPosition: number
+  mentionable: boolean
+  permissions: bigint
+  icon: string | null
+  unicodeEmoji: string | null
+}
+
+const RoleSchema = new Schema<Role>({
   oldId: { type: String, required: true },
   name: { type: String, required: true },
-  parentId: { type: String },
+  color: { type: Number, required: true },
+  hoist: { type: Boolean, required: true },
+  rawPosition: { type: Number, required: true },
+  mentionable: { type: Boolean, required: true },
+  permissions: { type: BigInt, required: true },
+  icon: String,
+  unicodeEmoji: String
+})
+
+interface Channel {
+  oldId: string
+  name: string
+  parentId: string | null
+  position: number
+  type: number
+  nsfw: boolean | null
+  rateLimitPerUser: number | null
+  topic: string | null
+  bitrate: number | null
+  rtcRegion: string | null
+  userLimit: number | null
+  videoQualityMode: number | null
+}
+
+const ChannelSchema = new Schema<Channel>({
+  oldId: { type: String, required: true },
+  name: { type: String, required: true },
+  parentId: String,
   position: { type: Number, required: true },
   type: { type: Number, required: true },
   nsfw: Boolean,
@@ -25,9 +67,24 @@ const ChannelSchema = new Schema({
   videoQualityMode: Number
 })
 
-export const BackupModel = model('backup', new Schema({
+export interface Backup {
+  id: string
+  guild: Guild
+  user: Types.ObjectId
+  roles: Role[]
+  channels: Channel[]
+  createdAt: Date
+}
+
+export const BackupModel = model<Backup>('Backup', new Schema({
   id: { type: String, default: generateCode },
-  guildId: { type: String, required: true },
-  user: { type: Types.ObjectId, required: true, ref: 'User' },
+  guild: GuildSchema,
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+  roles: [RoleSchema],
   channels: [ChannelSchema]
+}, {
+  timestamps: {
+    createdAt: true,
+    updatedAt: false
+  }
 }))

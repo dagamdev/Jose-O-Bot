e1d01aad78f71c073acc32b0824fd8958512ed31
Dagadev <gacub20ru@gmail.com>
2024-01-27 21:57:17 -0600

    Add databaseConnectionReady function

---
+++ src/lib/db.ts
@@ -0,0 +1,19 @@
+import { UserModel } from '../models'
+import { CACHE } from '../utils/data'
+import { type Backup } from '../models/backup'
+
+export function databaseConnectionReady () {
+  UserModel.find().populate<{ backups: Backup[] }>('backups').then(users => {
+    users.forEach(user => {
+      CACHE.backupIDs.push({
+        userId: user.userId,
+        IDs: user.backups.map((backup) => {
+          return {
+            value: backup.id,
+            name: `${backup.guild.name} | ${backup.createdAt.toLocaleString()} | ${backup.id}`
+          }
+        })
+      })
+    })
+  })
+}

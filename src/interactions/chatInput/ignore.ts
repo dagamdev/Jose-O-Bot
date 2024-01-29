be26e612288c1f53b6507a74ce7cabd3839b7d2e
Dagadev <gacub20ru@gmail.com>
2024-01-29 13:47:48 -0600

    Update command struct & Add subcommands logic

--- src/interactions/chatInput/ignore.ts
+++ src/interactions/chatInput/ignore.ts
@@ -1,5 +1,6 @@
 import { ClientSlashCommand } from '../../client'
-import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js'
+import { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder, GuildChannel } from 'discord.js'
+import { UserModel } from '../../models'
 
 const IgnoreScb = new SlashCommandBuilder()
   .setName('ignore')
@@ -17,12 +18,26 @@ const IgnoreScb = new SlashCommandBuilder()
     .setNameLocalization('es-ES', 'agregar')
     .setDescription('Add a new channel to the ignore list.')
     .setDescriptionLocalization('es-ES', 'Agrega un nuevo canal a la lista de ignorados.')
+    .addChannelOption(channel => channel
+      .setName('channel')
+      .setNameLocalization('es-ES', 'canal')
+      .setDescription('Channel to exclude from the backup.')
+      .setDescriptionLocalization('es-ES', 'Canal a excluir del respaldo.')
+      .setRequired(true)
+    )
   )
   .addSubcommand(remove => remove
     .setName('remove')
     .setNameLocalization('es-ES', 'eliminar')
     .setDescription('Remove a channel from the ignore list.')
     .setDescriptionLocalization('es-ES', 'Eliminar un canal de la lista de ignorados.')
+    .addChannelOption(channel => channel
+      .setName('channel')
+      .setNameLocalization('es-ES', 'canal')
+      .setDescription('Channel to include in the backup.')
+      .setDescriptionLocalization('es-ES', 'Canal a incluir en el respaldo.')
+      .setRequired(true)
+    )
   )
   .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
   .toJSON()
@@ -31,7 +46,94 @@ export default class BackupSlashCommand extends ClientSlashCommand {
   constructor () {
     super(IgnoreScb,
       async (int, client) => {
+        const { user, guildId, guild, options } = int
+        const subcommandName = options.getSubcommand(true)
+        const userData = await UserModel.findOne({ userId: user.id })
 
+        if (guildId === null || guild === null) {
+          int.reply({ ephemeral: true, content: 'Este comando solo se puede ejecutar dentro de un servidor.' })
+          return
+        }
+
+        if (subcommandName === 'list') {
+          const IgnoreListEmbed = new EmbedBuilder({
+            title: 'Lista de canales a excluir del respaldo',
+            description: userData === null ? '*Sin canales*' : userData.ignoreChannels.find(f => f.guildId === guildId)?.channelIDs.map(id => `<#${id}>`).join('\n') ?? '*Sin canales*'
+          }).setColor(client.data.colors.default)
+
+          int.reply({ ephemeral: true, embeds: [IgnoreListEmbed] })
+        }
+
+        if (subcommandName === 'add') {
+          const channel = options.getChannel('channel', true)
+
+          if (userData === null) {
+            UserModel.create({
+              userId: user.id,
+              ignoreChannels: [{
+                guildId,
+                channelIDs: [channel.id]
+              }]
+            })
+          } else {
+            const guildChannels = userData.ignoreChannels.find(ic => ic.guildId === guildId)
+
+            if (guildChannels === undefined) {
+              userData.ignoreChannels.push({
+                guildId,
+                channelIDs: [channel.id]
+              })
+            } else {
+              if (channel instanceof GuildChannel && guildChannels.channelIDs.some(s => s === channel.parentId)) {
+                int.reply({ ephemeral: true, content: `El canal proporcionado (<#${channel.id}>) ya está en la lista de canales excluidos del respaldo debido a su categoría.` })
+                return
+              }
+
+              guildChannels.channelIDs.push(channel.id)
+            }
+
+            await userData.save()
+          }
+
+          const IgnoreAddEmbed = new EmbedBuilder({
+            title: 'Canal agregado',
+            description: `El canal **<#${channel.id}>** se ha agregado a tu lista de canales a excluir del respaldo.`
+          }).setColor('Green')
+
+          int.reply({ ephemeral: true, embeds: [IgnoreAddEmbed] })
+        }
+
+        if (subcommandName === 'remove') {
+          const channel = options.getChannel('channel', true)
+
+          if (userData === null) {
+            int.reply({ ephemeral: true, content: 'No puedes eliminar ningún canal de la lista de canales a excluir del respaldo porque no tienes una lista.' })
+            return
+          }
+
+          const guildChannels = userData.ignoreChannels.find(ic => ic.guildId === guildId)
+
+          if (guildChannels === undefined) {
+            int.reply({ ephemeral: true, content: '!Tu lista de canales a excluir del respaldo está vacía¡' })
+            return
+          }
+
+          if (!guildChannels.channelIDs.some(s => s === channel.id)) {
+            int.reply({ ephemeral: true, content: `El canal proporcionado *(<#${channel.id}>)* no pertenece a tu lista de canales a excluir del respaldo.` })
+            return
+          }
+
+          guildChannels.channelIDs.splice(guildChannels.channelIDs.indexOf(channel.id), 1)
+
+          await userData.save()
+
+          const IgnoreRemoveEmbed = new EmbedBuilder({
+            title: 'Canal eliminado',
+            description: `El canal **<#${channel.id}>** se ha eliminado de tu lista de canales a excluir del respaldo.`
+          }).setColor('Green')
+
+          int.reply({ ephemeral: true, embeds: [IgnoreRemoveEmbed] })
+        }
       }
     )
   }

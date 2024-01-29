8b786f6eaf2621a1472f306201ddd675bcec6462
Dagadev <gacub20ru@gmail.com>
2024-01-29 13:47:01 -0600

    Add ignoreChannels property

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,13 +3,21 @@ import { model, Schema, type Types } from 'mongoose'
 interface User {
   userId: string
   backups: Types.ObjectId[]
+  ignoreChannels: Array<{
+    guildId: string
+    channelIDs: string[]
+  }>
   createdAt: Date
   updatedAt: Date
 }
 
 export const UserModel = model<User>('User', new Schema({
   userId: { type: String, required: true },
-  backups: [{ type: Schema.Types.ObjectId, ref: 'Backup' }]
+  backups: [{ type: Schema.Types.ObjectId, ref: 'Backup' }],
+  ignoreChannels: [{
+    guildId: { type: String, required: true },
+    channelIDs: [{ type: String, required: true }]
+  }]
 }, {
   timestamps: true
 }))

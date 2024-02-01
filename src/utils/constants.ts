0f0c4def12cf9a21ae892f61249a703d4156e7b5
Dagadev <gacub20ru@gmail.com>
2024-01-31 22:08:44 -0600

    Update enum

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,9 @@ export enum BUTTON_IDS {
   CREATE_BACKUP_CONFIRM = 'create-backup-confirm',
   CREATE_BACKUP_CANCEL = 'create-backup-cancel',
   LOAD_BACKUP_CONFIRM = 'load-backup-confirm',
-  LOAD_BACKUP_CANCEL = 'load-backup-cancel'
+  LOAD_BACKUP_CANCEL = 'load-backup-cancel',
+  YES_CHECK_VERIFICATIONS = 'yes-check-verifications',
+  NO_CHECK_VERIFICATIONS = 'no-check-verifications'
 }
 
 export enum CHANNEL_IDS {
